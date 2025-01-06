35374e56485fb007ef18f685ae9b11c134a74377
lifegatejustice <lifegatedetom@gmail.com>
2025-01-06 14:53:02 +0100

    `Added search icon link, updated search bar, added dynamic image section, and implemented image change functionality`

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,40 @@
-document.getElementById('menu').addEventListener('click', () => {
+document.getElementById('menu').addEventListener('click', toggleMenu);
+document.getElementById('search-icon-link').addEventListener('click', toggleMenu);
+
+function toggleMenu() {
     const nav = document.querySelector('.navigation');
     const menu = document.getElementById('menu');
     nav.classList.toggle('open');
     menu.classList.toggle('open');
-});
+}
+
+
+const image = document.getElementById('myImage');
+const images = [
+    "images/catfish.webp",
+    "images/turkey.webp",
+    "images/plantain.webp",
+    "images/tomatoes.webp",
+    "images/cucumber.webp",
+    "images/maize.webp",
+    "images/goat.webp"
+];
+let currentIndex = 0;
+
+function changeImage() {
+    // Add the swipe-left class for animation
+    image.classList.add('swipe-left');
+    
+    // Wait for the animation to complete before changing the image
+    setTimeout(() => {
+        // Update the image source
+        currentIndex = (currentIndex + 1) % images.length;
+        image.src = images[currentIndex];
+        
+        // Reset the animation class
+        image.classList.remove('swipe-left');
+    }, 500); // Match the CSS transition duration
+}
+
+// Automatically change the image every 3 seconds
+setInterval(changeImage, 5000);
